3e1c41a0779c060d98496c1186308e1e64dc9c2f
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-06-28 13:30:56 -0300

    start

---
+++ ranking.hs
@@ -0,0 +1,9 @@
+import Data.Sequence
+
+addInRank :: (String, Int) -> [(String, Int)] -> [(String, Int)]
+(fromList [] |> (String, Int)) 
+
+ranking_easy = []
+ranking_medium = []
+ranking_hard = []
+
