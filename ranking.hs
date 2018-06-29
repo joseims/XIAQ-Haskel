6e870e28b78e49a9b6e432782ef0fb938f0fa72b
Hiago Sousa <hiago.sousa@ccc.ufcg.edu.br>
2018-06-29 15:13:19 -0300

    modified rank

--- ranking.hs
+++ ranking.hs
@@ -19,12 +19,12 @@ addInRank name_points my_rank = my_rank ++ [name_points]
 orderedRank :: [(String, Int)] -> [(String, Int)]
 orderedRank rank = sortOn snd rank 
 
--- Inserir a pontuacao no rank mapeado  
-insert :: (String, Int) -> Int -> [(Int, [(String, Int)])] -> [(Int, [(String, Int)])]
-insert name_points level rank | level == 1 then orderedRank $ addInRank name_points rank 
-                              | level == 2 then orderedRank $ addInRank name_points rank
-                              | level == 3 then orderedRank $ addInRank name_points rank
-
 -- Selecionar as três ultimas pontuacoes
 getThreeLargest :: [(String, Int)] -> [(String, Int)]
 getThreeLargest rank = take 3 $ reverse rank
+
+-- Inserir a pontuacao no rank mapeado  
+insert :: (String, Int) -> Int -> [(Int, [(String, Int)])] -> [(Int, [(String, Int)])]
+insert name_points level rank | level == 1 then getThreeLargest $ orderedRank $ addInRank name_points rank 
+                              | level == 2 then getThreeLargest $ orderedRank $ addInRank name_points rank
+                              | level == 3 then getThreeLargest $ orderedRank $ addInRank name_points rank
