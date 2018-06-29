a6a9eddf2cf940356b6212443fcbf8a79af3cf6c
Hiago Sousa <hiago.sousa@ccc.ufcg.edu.br>
2018-06-29 14:26:08 -0300

    start rank

--- ranking.hs
+++ ranking.hs
@@ -1,7 +1,16 @@
-import Data.Sequence
+module ranking
+( 
+nome, 
+pontuacao
+)
 
-addInRank :: (String, Int) -> [(String, Int)] -> [(String, Int)]
-(fromList [] |> (String, Int)) 
+where
+
+import Data.Sequence
+import qualified Data.Map as Map
+
+insert :: (String, Int) -> Int -> [(String, Int)] -> [(String, Int)]
+insert name&points dificulty rank | dificulty == 1 = 
 
 ranking_easy = []
 ranking_medium = []
