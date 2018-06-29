404d15a3b50df71060650349f9fc9afe09bc9335
Hiago Sousa <hiago.sousa@ccc.ufcg.edu.br>
2018-06-29 15:09:44 -0300

    modified rank

--- ranking.hs
+++ ranking.hs
@@ -1,18 +1,30 @@
 module ranking
 ( 
-nome, 
-pontuacao
+name, 
+points,
+level
 )
 
 where
 
 import Data.Sequence
 import qualified Data.Map as Map
+import Data.List
 
-insert :: (String, Int) -> Int -> [(String, Int)] -> [(String, Int)]
-insert name&points dificulty rank | dificulty == 1 = 
+-- Adicionar no array, tupla com nome e pontuacao, array com o ranking
+addInRank :: (String, Int) -> [(String, Int)] -> [(String, Int)]
+addInRank name_points my_rank = my_rank ++ [name_points]
 
-ranking_easy = []
-ranking_medium = []
-ranking_hard = []
+-- Ordenando Rank
+orderedRank :: [(String, Int)] -> [(String, Int)]
+orderedRank rank = sortOn snd rank 
 
+-- Inserir a pontuacao no rank mapeado  
+insert :: (String, Int) -> Int -> [(Int, [(String, Int)])] -> [(Int, [(String, Int)])]
+insert name_points level rank | level == 1 then orderedRank $ addInRank name_points rank 
+                              | level == 2 then orderedRank $ addInRank name_points rank
+                              | level == 3 then orderedRank $ addInRank name_points rank
+
+-- Selecionar as três ultimas pontuacoes
+getThreeLargest :: [(String, Int)] -> [(String, Int)]
+getThreeLargest rank = take 3 $ reverse rank
