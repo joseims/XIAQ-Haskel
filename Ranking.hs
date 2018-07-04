1d2692dc91060c28c6b380cd7a4700a674aeaa53
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-07-04 13:41:15 -0300

    more changes

--- Ranking.hs
+++ Ranking.hs
@@ -1,14 +1,9 @@
 module Ranking
 ( 
-name, 
-points,
-level
-)
-
-where
+insert
+) where
 
 import Data.Sequence
-import qualified Data.Map as Map
 import Data.List
 
 -- Adicionar no array, tupla com nome e pontuacao, array com o ranking
@@ -24,7 +19,5 @@ getThreeLargest :: [(String, Int)] -> [(String, Int)]
 getThreeLargest rank = take 3 $ reverse rank
 
 -- Inserir a pontuacao no rank mapeado  
-insert :: (String, Int) -> Int -> [(Int, [(String, Int)])] -> [(Int, [(String, Int)])]
-insert name_points level rank | level == 1 then getThreeLargest $ orderedRank $ addInRank name_points rank 
-                              | level == 2 then getThreeLargest $ orderedRank $ addInRank name_points rank
-                              | level == 3 then getThreeLargest $ orderedRank $ addInRank name_points rank
+insert :: (String, Int) -> [(String, Int)] -> [(String, Int)]
+insert name_points rank = getThreeLargest $ orderedRank $ addInRank name_points rank 
