d60746106b87b94fe9d374ec9b3415b72226ec0a
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-07-04 14:14:17 -0300

    start store

---
+++ Store.hs
@@ -0,0 +1,8 @@
+import Item
+
+module Store
+(
+) where
+
+text = [("armor"), ("weapon"), ("Poção (Recupera 15 de vida)"), ("Bem vindo a Loja, em que podemos ajudá-lo?"), ("Você já comprou esse item, escolha novamente"), ("Obrigado e volte sempre!"), ("Sinto muito, você não me parece ter dinheiro suficiente para comprar isso"), ("Opção Inválida, escolha novamente")]
+
