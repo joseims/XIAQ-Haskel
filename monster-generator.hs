d434e78fc3e506acf5642d54a4291266bac67628
José Ignácio <jose.schmid@ccc.ufcg.edu.br>
2018-07-05 23:44:55 -0300

    item and monsters finished

--- monster-generator.hs
+++ monster-generator.hs
@@ -1,28 +1,41 @@
+import System.Random
+
 data Monster = Monster { 
     strength :: Int,
-    defense :: Int,
     health :: Int,
     name :: String
 } deriving (Show)
 
 
-
-rand :: Int
-rand = 0
-
-
-randMonsterName ::  String
-randArmorName = ["Ladão","Dragão","Golem","Gosma", "Vampiro","Lobisomen","Rato gigante"]) !! rand 
+rand ::Int -> IO Int
+rand n = randomRIO (0,n)
 
 
+randMonsterName :: IO String
+randMonsterName = do 
+    i <- rand 6
+    return (["Ladrao","Dragao","Golem","Gosma", "Vampiro","Lobisomen","Rato gigante"] !! i)
 
 
-randMonster:: Monster
-randArmor = Monster randStatus randStatus randStatus (randArmorName++randAlterer)
+monsterFullName :: IO String 
+monsterFullName = do
+    name <- randMonsterName
+    alterer <- randAlterer
+    return (name++alterer) 
+
+randMonster:: Int -> IO Monster
+randMonster gpm = do
+    strRand <- rand 30
+    hpRand <- rand 15
+    price <- rand 15
+    name <- monsterFullName 
+    return (Monster (randStatus strRand 10 gpm) (randStatus hpRand 7 gpm) name)
 
 
-randStatus :: Int
-randStatus = (15 + rand)
+randStatus :: Int -> Int -> Int -> Int
+randStatus rando n  gpm = (n + rando) + (((n + rando)*gpm) `div` 5)
 
-randAlterer :: String
-randAlterer =  [" Pacifista"," Burro"," Cego"," Imaginário"," Sem Pernas", " Invisivel", " Gigantesco"," de 3 cabeças", " Aterrorizante"," Cabeludo"," Rochoso"," Assassino", ""] !! rand
+randAlterer :: IO String
+randAlterer = do
+    i <- rand 12
+    return ([" Pacifista"," Burro"," Cego"," Imaginario"," Sem Pernas", " Invisivel", " Gigantesco"," de 3 cabecas", " Aterrorizante"," Cabeludo"," Rochoso"," Assassino", ""] !! i)
